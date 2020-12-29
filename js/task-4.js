ea6bff530f088e0ab2b1d58e5137f4192026857c
Klem26 <Kankina26@gmail.com>
2020-12-29 16:33:18 +0200

    Create task-4.js

---
+++ js/task-4.js
@@ -0,0 +1,29 @@
+
+let credits = 23580;
+const pricePerDroid = 3000;
+
+
+
+let userInput = prompt('Какое количество дроидов хотите купить?')
+userInput = Number(userInput);
+
+ let totalPrice = userInput * pricePerDroid;
+console.log(totalPrice);
+
+if (userInput === null) {
+    console.log('Отменено пользователем!');
+
+} else if (credits >= totalPrice) {
+    credits = credits - totalPrice ; 
+    console.log(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`);
+} else if (credits < totalPrice) {
+    console.log('Недостаточно средств на счету!');
+    
+} 
+
+
+    
+
+
+   
+
