564827f2cda16c140badd88df2fd0cb51636d3b8
Klem26 <Kankina26@gmail.com>
2021-01-05 18:04:54 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,32 +1,78 @@
-// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-//   let message;
-//   // Пиши код ниже этой строки
-// let totalPrice = orderedQuantity * pricePerDroid;
-  
-//   if( totalPrice >=customerCredits ){
-//       console.log  ('Недостаточно средств на счету!')
-      
-//   }else if (customerCredits= customerCredits - totalPrice){
-//       console.log(`Вы купили ${orderedQuantity} дроидов, на счету осталось${customerCredits}  кредитов`)
-//   }else{
-//   massage ='Вы купили <число> дроидов, на счету осталось <число> кредитов'}
-//   // Пиши код выше этой строки
-//     return message;
-    
-// } 
-// makeTransaction(2000, 8, 10000)
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
 
-// makeTransaction(3000, 5, 23000)
+  let message;
+
+let totalPrice = orderedQuantity * pricePerDroid;
+  
+  if (totalPrice >= customerCredits) {
+    
+      console.log  ('Недостаточно средств на счету!')
+      
+  } else if (customerCredits = customerCredits - totalPrice) {
+    
+
+      console.log(`Вы купили ${orderedQuantity} дроидов, на счету осталось${customerCredits}  кредитов`)
+  } else {
+
+  massage ='Вы купили <число> дроидов, на счету осталось <число> кредитов'}
+
+    return message;
+    
+} 
+makeTransaction(2000, 8, 10000)
+
+makeTransaction(3000, 5, 23000)
 
 
 
 function checkIfCanAccessContent(subType) {
-  const canAccessContent = (subType='pro')||(subType ='vip'); // дополни эту строку
+  const canAccessContent = (subType==='pro')||(subType ==='vip'); // дополни эту строку
 
-    // return canAccessContent;
-    console.log(canAccessContent)
+    return canAccessContent;
+    // console.log(canAccessContent)
 }
 
 checkIfCanAccessContent('pro')
 checkIfCanAccessContent('vip')
 checkIfCanAccessContent('free')
+
+
+function getShippingCost(country) {
+  let message;
+  
+  
+  switch (country) {
+  case  'Китай':
+  price = '100';
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+    break;
+
+  case 'Чили':
+    price = '250';
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+
+    break;
+        case 'Австралия':
+        price = '170';
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+
+    break;
+        case 'Ямайка':
+        price = '120';
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+  
+    break;
+
+  default:
+    message= 'Извините, в вашу страну доставки нет';
+}
+  
+ 
+  return message;
+}
+
+getShippingCost('Австралия');
+getShippingCost('Германия');
+getShippingCost('Китай');
+getShippingCost('Ямайка');
+getShippingCost('Чили');
