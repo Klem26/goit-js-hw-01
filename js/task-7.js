3b84ac5ba16676d22b2ef578b045971ea5afce54
Klem26 <Kankina26@gmail.com>
2021-01-05 18:05:01 +0200

    Create task-7.js

---
+++ js/task-7.js
@@ -0,0 +1,9 @@
+function getNameLength(name) {
+  const message = `Длина вашего имени ${name.length} символа(ов)`; 
+
+  return message;
+}
+
+getNameLength('Poly');
+getNameLength('Harambe');
+getNameLength('Billy');
