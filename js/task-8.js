214773fabf05b3dae49ecd8c8d460849594f8c8a
Klem26 <Kankina26@gmail.com>
2021-01-05 18:05:05 +0200

    Create task-8.js

---
+++ js/task-8.js
@@ -0,0 +1,19 @@
+const courseTopic = 'JavaSript для начинающих';
+
+
+const courseTopicLength = courseTopic.length;
+
+const firstElement = courseTopic[0];
+
+const lastElement = courseTopic[courseTopic.length - 1];
+
+
+
+// Объявлена переменная courseTopic.
+// Значение переменной courseTopic это строка 'JavaSript для начинающих'.
+// Объявлена переменная courseTopicLength.
+// Значение переменной courseTopicLength это число 24.
+// Объявлена переменная firstElement.
+// Значение переменной firstElement это строка 'J'.
+// Объявлена переменная lastElement.
+// Значение переменной lastElement это строка 'х'.
