87e1334548bc9129103e53e50bcce3a8f17a3900
Klem26 <Kankina26@gmail.com>
2021-01-05 18:04:47 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -4,10 +4,12 @@ let message;
 const userInput = prompt('Введите пароль');
  
 if (userInput === null) {
-    message ='Отменено пользователем!';
+    message = 'Отменено пользователем!';
+    
 
 } else if (userInput === ADMIN_PASSWORD) {
     message = 'Добро пожаловать!';
+    
 } else {
      message ='Доступ запрещен, неверный пароль!'   
   }
