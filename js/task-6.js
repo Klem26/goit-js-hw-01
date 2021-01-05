1c90e3e07c3fdcb87499007076d922a35ffe644b
Klem26 <Kankina26@gmail.com>
2021-01-05 18:04:58 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -2,19 +2,26 @@ let input;
 let total = 0;
 
 while (true) {
+
     let input = prompt('введи число');
 
     if (input === null) {
+
         console.log('Отменено пользователем!');
         break;
     }
+
     input = Number(input);
+
     const notANumber = Number.isNaN(input);
 
     if (notANumber) {
+
         alert('Было введено не число, попробуйте еще раз');
         continue;
     }
+
     total+=input
 }
+
 alert(`Общая сумма чисел равна  ${total}`);
